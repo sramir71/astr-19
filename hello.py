5d920f2ecd97b1f9e067c7941cf10f68a1f4c253
sramir71 <156133472+sramir71@users.noreply.github.com>
2024-01-09 12:48:56 -0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+# assignment: programming assignment 1
+# author: Saul Ramirez
+# date: 10/4/2023
+# file: hello.py is a program that asks the user to enter user's name,
+# major, and age movie and outputs a greeting message that
+# include the information about the user
+# input: string data
+# output: string data
+# Taking input from the user as integer
+# Taking input from the user as integer
+# Taking input from the user
+
+name = input("Hello! What is your name?\n")
+print("Nice to meet you, " + name + "!\n")
+major = input("What is your major?\n")
+print(f"Your major is " + major + ".")
+age = input("What is your age? ")
+myage = 18 + 1
+print(f"You will be" ,myage, "years old next year.")
